b42cdad9107d8b48cf2383866b50b1684c392f37
Nikita Lebedev <nikita.lebedev@lazada.com>
2015-10-26 14:09:15 +0300

    small refactor and checks

--- demo/js/jquery.switcher.js
+++ demo/js/jquery.switcher.js
@@ -1,7 +1,6 @@
 ﻿(function($) {
   $.fn.switcher = function() {
     var checkbox = {
-      // Events
       initialize: function(self) {
         var span = self.children(".js__s_checkbox").children("span");
         var left = self.children(".js__s_label_left");
@@ -22,10 +21,9 @@
         });
       },
 
-      // Switch checkbox
+
       change: function(self) {
         var parent = self.closest(".js__s_checkbox");
-        // We learn to switch and lock in a pinch if you have an active
         if (parent.hasClass("js__s_left") && self.hasClass("js__s_noactive")) {
           checkbox.toRight(parent);
         }
@@ -34,10 +32,8 @@
         }
       },
 
-      // Switch left
       toLeft: function(self) {
         var parent = self.closest(".js__s_checkbox");
-        // Toggle switch only if in the right position
         if (parent.hasClass("js__s_right")) {
           parent
             .removeClass("js__s_right")
@@ -46,10 +42,8 @@
         }
       },
 
-      // Switch right
       toRight: function(self) {
         var parent = self.closest(".js__s_checkbox");
-        // Toggle switch only if in the left position
         if (parent.hasClass("js__s_left")) {
           parent
             .removeClass("js__s_left")
@@ -58,8 +52,8 @@
         }
       },
 
+
       help: {
-        // Helper function that swaps the spans
         switchSpan: function(parent) {
           var active = parent.children(".js__s_active");
           var noactive = parent.children(".js__s_noactive");
@@ -73,11 +67,10 @@
       }
     };
 
-    // In loop looking for what is called
+
     return this.each(function() {
       var self = $(this);
       checkbox.initialize(self);
     });
   };
 })(jQuery);
-
--- jquery.switcher.coffee
+++ jquery.switcher.coffee
@@ -1,7 +1,6 @@
 (($) ->
   $.fn.switcher = ->
     checkbox =
-      # Events
       initialize: (self) ->
         span = self.children('.js__s_checkbox').children('span')
         left = self.children('.js__s_label_left')
@@ -21,31 +20,24 @@
           checkbox.toRight(span)
         )
 
-      # Switch checkbox
+
       change: (self) ->
         parent = self.closest('.js__s_checkbox')
-        # We learn to switch and lock in a pinch if you have an active
         if parent.hasClass('js__s_left') && self.hasClass('js__s_noactive')
           checkbox.toRight(parent)
         else if parent.hasClass('js__s_right') && self.hasClass('js__s_noactive')
           checkbox.toLeft(parent)
 
-
-      # Switch left
       toLeft: (self) ->
         parent = self.closest('.js__s_checkbox')
-        # Toggle switch only if in the right position
         if parent.hasClass('js__s_right')
           parent
             .removeClass('js__s_right')
             .addClass('js__s_left')
           checkbox.help.switchSpan(parent)
 
-
-      # Switch right
       toRight: (self) ->
         parent = self.closest('.js__s_checkbox')
-        # Toggle switch only if in the left position
         if parent.hasClass('js__s_left')
           parent
             .removeClass('js__s_left')
@@ -54,7 +46,6 @@
 
 
       help:
-        # Helper function that swaps the spans
         switchSpan: (parent) ->
           active = parent.children('.js__s_active')
           noactive = parent.children('.js__s_noactive')
@@ -66,10 +57,8 @@
             .addClass('js__s_active')
 
 
-    # In loop looking for what is called
     @each( ->
       self = $(this)
       checkbox.initialize(self)
     )
 )(jQuery)
-
